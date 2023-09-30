94c333f95b18ef4c8e61bbd0aa9b7521b8edad35
Sourav Dey <deysourav756@gmail.com>
2023-09-30 17:31:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require("express");
 
@@ -14,20 +14,18 @@ app.get("/twitter", (req, res) => {
   res.send("https://twitter.com/Srv228");
 });
 
-app.get("/login" , (req, res) => {
-     res.send('<h1>Please login at chai aur code</h1>')
-})
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai aur code</h1>");
+});
 
-app.get('/youtube' , (req, res) =>{
-    res.send("<h2>Chai aur Code</h2>")
-})
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai aur Code</h2>");
+});
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
 // });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-
-
+  console.log(`Example app listening on port ${port}`);
+});
